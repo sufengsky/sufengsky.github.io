c114511a6460a71bed3e377ffb5d2791bd92d7f8
sufengsky <365490686@qq.com>
2017-07-27 13:09:54 +0800

    a

---
+++ xjxx/xjxx/js/common.js
@@ -0,0 +1,8 @@
+﻿//显示提示信息对话框 1 秒后消失
+//message:要显示的信息
+function showMessage(message) {
+    var info = "<div class='ac_toast' id='toast-default' style='display:block;'><div class='ac_toast_backdrop'></div>" +
+    "<div class='ac_toast_dialog'><div class='ac_toast_content'><span>" + message + "</span></div></div></div>";
+    $("body").append(info);
+    setTimeout(function () { $("#toast-default").remove() }, 1000);
+}
